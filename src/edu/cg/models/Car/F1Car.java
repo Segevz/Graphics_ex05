1b31d7e89d37a4c6d32500fd0af067978d770801
Yarden Glasberg <yarden_gl@hotmail.com>
2019-06-02 11:30:26 +0300

    F1Car

--- src/edu/cg/models/Car/F1Car.java
+++ src/edu/cg/models/Car/F1Car.java
@@ -12,8 +12,27 @@ public class F1Car implements IRenderable {
 
 	@Override
 	public void render(GL2 gl) {
-		// TODO: Render the whole car. 
-		//       Here You should only render the three parts: back, center and front.
+		// Render the whole car. 
+		// Here You should only render the three parts: back, center and front.
+		
+		Back back = new Back();
+		Center center = new Center();
+		Front front = new Front();
+
+		// Render back
+		gl.glTranslated((-1) * (Specification.B_LENGTH + Specification.C_BASE_LENGTH) / 2, 0, 0);
+		back.render(gl);
+		
+		// Render center
+		gl.glPushMatrix();
+		center.render(gl);
+
+
+		// render front
+		gl.glTranslated((Specification.F_FRONT_LENGTH + Specification.B_LENGTH) / 2 + Specification.C_BASE_LENGTH, 0, 0);
+		front.render(gl);
+
+		gl.glPopMatrix();
 	}
 
 	@Override
